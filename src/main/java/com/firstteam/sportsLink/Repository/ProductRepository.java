07214d89e9a039f6d67ae0cd706f1d16e712ef55
sangwoo_K <lyly403@daum.net>
2024-05-26 01:38:12 +0900

    Remove folder from local repository

--- src/main/java/com/firstteam/sportsLink/Repository/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package com.firstteam.sportsLink.Repository;
-
-import com.firstteam.sportsLink.Entity.ProductEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<ProductEntity, Long> {
-
-}
