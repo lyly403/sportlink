1ea85f136731164dd8a216d03735b5768d49f677
jscript2217 <dlwjdtlr6@naver.com>
2024-05-27 11:04:52 +0900

    Revert "MemberRepository 누락 추가"
    This reverts commit 4cadb6189661e5e83fcefe9207407d8e7bc2a0fd.

--- src/main/java/com/firstteam/sportsLink/Member/MemberRepository.java
+++ src/main/java/com/firstteam/sportsLink/Member/MemberRepository.java
@@ -1,11 +1,14 @@
 package com.firstteam.sportsLink.Member;
 
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
+import java.lang.reflect.Member;
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<MemberEntity, String> {
-    boolean existsByUserid(String userid);
+    boolean existsById(String id);
     Page<MemberEntity> findAll(Pageable pageable);
-    MemberEntity findByUserid(String userId); // userid 컬럼을 기반으로 사용자 찾기
 }
