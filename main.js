b068a3f058d824080de5f3492b55eba7a350962c
JaeWOOKJ1 <wjdwodnr97@gmail.com>
2024-05-28 15:48:37 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,281 @@
+
+(function() {
+  "use strict";
+
+  /** Navbar links active state on scroll */
+  let navbarlinks = document.querySelectorAll('#navbar a');
+
+  function navbarlinksActive() {
+    navbarlinks.forEach(navbarlink => {
+
+      if (!navbarlink.hash) return;
+
+      let section = document.querySelector(navbarlink.hash);
+      if (!section) return;
+
+      let position = window.scrollY + 200;
+
+      if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
+        navbarlink.classList.add('active');
+      } else {
+        navbarlink.classList.remove('active');
+      }
+    })
+  }
+  window.addEventListener('load', navbarlinksActive);
+  document.addEventListener('scroll', navbarlinksActive);
+
+  /** Mobile nav toggle */
+  const mobileNavShow = document.querySelector('.mobile-nav-show');
+  const mobileNavHide = document.querySelector('.mobile-nav-hide');
+
+  document.querySelectorAll('.mobile-nav-toggle').forEach(el => {
+    el.addEventListener('click', function(event) {
+      event.preventDefault();
+      mobileNavToogle();
+    })
+  });
+
+  function mobileNavToogle() {
+    document.querySelector('body').classList.toggle('mobile-nav-active');
+    mobileNavShow.classList.toggle('d-none');
+    mobileNavHide.classList.toggle('d-none');
+  }
+
+  /** Hide mobile nav on same-page/hash links */
+  document.querySelectorAll('#navbar a').forEach(navbarlink => {
+
+    if (!navbarlink.hash) return;
+
+    let section = document.querySelector(navbarlink.hash);
+    if (!section) return;
+
+    navbarlink.addEventListener('click', () => {
+      if (document.querySelector('.mobile-nav-active')) {
+        mobileNavToogle();
+      }
+    });
+
+  });
+
+  /** Toggle mobile nav dropdowns */
+  const navDropdowns = document.querySelectorAll('.navbar .dropdown > a');
+
+  navDropdowns.forEach(el => {
+    el.addEventListener('click', function(event) {
+      if (document.querySelector('.mobile-nav-active')) {
+        event.preventDefault();
+        this.classList.toggle('active');
+        this.nextElementSibling.classList.toggle('dropdown-active');
+
+        let dropDownIndicator = this.querySelector('.dropdown-indicator');
+        dropDownIndicator.classList.toggle('bi-chevron-up');
+        dropDownIndicator.classList.toggle('bi-chevron-down');
+      }
+    })
+  });
+
+  /** Scroll top button */
+  const scrollTop = document.querySelector('.scroll-top');
+  if (scrollTop) {
+    const togglescrollTop = function() {
+      window.scrollY > 100 ? scrollTop.classList.add('active') : scrollTop.classList.remove('active');
+    }
+    window.addEventListener('load', togglescrollTop);
+    document.addEventListener('scroll', togglescrollTop);
+    scrollTop.addEventListener('click', window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    }));
+  }
+
+  /** Initiate glightbox  */
+  const glightbox = GLightbox({
+    selector: '.glightbox'
+  });
+
+  /** Initiate pURE cOUNTER */
+  new PureCounter();
+
+  /** Init swiper slider with 1 slide at once in desktop view */
+  new Swiper('.slides-1', {
+    speed: 600,
+    loop: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false
+    },
+    slidesPerView: 'auto',
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    }
+  });
+
+  /**  Init swiper slider with 3 slides at once in desktop view  */
+  new Swiper('.slides-3', {
+    speed: 600,
+    loop: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false
+    },
+    slidesPerView: 'auto',
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 40
+      },
+
+      1200: {
+        slidesPerView: 3,
+      }
+    }
+  });
+
+  /** Gallery Slider   */
+  new Swiper('.gallery-slider', {
+    speed: 400,
+    loop: true,
+    centeredSlides: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false
+    },
+    slidesPerView: 'auto',
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true
+    },
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 20
+      },
+      640: {
+        slidesPerView: 3,
+        spaceBetween: 20
+      },
+      992: {
+        slidesPerView: 5,
+        spaceBetween: 20
+      }
+    }
+  });
+
+  /** Animation on scroll function and init  */
+  function aos_init() {
+    AOS.init({
+      duration: 1000,
+      easing: 'ease-in-out',
+      once: true,
+      mirror: false
+    });
+  }
+  window.addEventListener('load', () => {
+    aos_init();
+  });
+
+// 날씨 //
+
+
+const API_KEY = "9cbefddcf0b1e2af09c146b2df82ad9c"; //add your API KEY
+const COORDS = 'coords'; //좌표를 받을 변수
+
+//DOM객체들
+const weatherInfo = document.querySelector('.weatherInfo');
+const weatherIconImg = document.querySelector('.weatherIcon');
+
+//초기화
+function init() {
+    askForCoords();
+}
+
+//좌표를 물어보는 함수
+function askForCoords() {
+    navigator.geolocation.getCurrentPosition(handleSuccess, handleError);
+}
+
+//좌표를 얻는데 성공했을 때 쓰이는 함수
+function handleSuccess(position) {
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    const coordsObj = {
+        latitude,
+        longitude
+    };
+    getWeather(latitude, longitude); //얻은 좌표값을 바탕으로 날씨정보를 불러온다.
+}
+//좌표를 얻는데 실패했을 때 쓰이는 함수
+function handleError() {
+    console.log("can't not access to location");
+}
+
+// 날씨 api를 통해 날씨에 관련된 정보들을 받아온다.
+function getWeather(lat, lon) {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric&lang=kr`)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (json) {
+            // 온도와 날씨 아이콘 정보를 받아온다.
+            const temperature = json.main.temp;
+            const weatherIcon = json.weather[0].icon;
+            const weatherDescription = convertWeatherDescription(json.weather[0].description); // 영문 설명을 한글로 변환
+
+            // 영문으로 받은 날씨 아이콘 정보를 한글로 변환한다.
+            const weatherIconCode = json.weather[0].icon;
+            const weatherIconAdrs = `https://openweathermap.org/img/wn/${weatherIcon}@2x.png`;
+
+            // 받아온 정보들을 표현한다.
+            const weatherIconHTML = `<img src="${weatherIconAdrs}" alt="Weather Icon" style="display: inline-block; vertical-align: middle;">`;
+            const temperatureHTML = `<span style="display: inline-block; vertical-align: middle; margin-right: 20px;">${temperature} °C</span>`;
+            const weatherDescriptionHTML = `<span style="display: inline-block; vertical-align: middle; margin-left: 20px;">${weatherDescription}</span>`;
+            weatherInfo.innerHTML = `${weatherIconHTML}${temperatureHTML}${weatherDescriptionHTML}`;
+        })
+        .catch((error) => console.log("error:", error));
+}
+
+// 영문으로 받은 날씨 설명을 한글로 변환하는 함수
+function convertWeatherDescription(description) {
+    switch (description) {
+        case "clear sky":
+            return "맑음";
+        case "few clouds":
+            return "구름 조금";
+        case "scattered clouds":
+            return "구름 많음";
+        case "broken clouds":
+            return "구름 낌";
+        case "shower rain":
+            return "소나기";
+        case "rain":
+            return "비";
+        case "thunderstorm":
+            return "천둥 번개";
+        case "snow":
+            return "눈";
+        case "mist":
+            return "안개";
+        default:
+            return description;
+    }
+}
+init();
+
+})();
