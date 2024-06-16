bbe65367409a9ce3b43cd3cc025d73e6df92a9e9
ennni <djfwkd35@gamil.com>
2024-06-16 21:12:16 +0900

    Update validate_service.sh to use domain address

--- scripts/validate_service.sh
+++ scripts/validate_service.sh
@@ -2,5 +2,5 @@
 
 # Validate service script
 echo "Validating service at $(date)"
-curl -f http://localhost:8080 || exit 1
+curl -f http://sportlink.store || exit 1
 echo "Service is up and running at $(date)"
