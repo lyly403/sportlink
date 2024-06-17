f7aa61b648c1aa98ff0eb7c041bd22346552c1a2
jscript2217 <dlwjdtlr6@naver.com>
2024-06-17 15:44:33 +0900

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -4,6 +4,7 @@
 echo "Before install script executed at $(date)" >> /home/ubuntu/first-team-project-dev/before_install.log
 
 # 필요한 디렉토리를 생성
+mkdir -p /home/ubuntu/build
 mkdir -p /home/ubuntu/first-team-project-dev
 mkdir -p /home/ubuntu/first-team-project-dev/scripts
 
