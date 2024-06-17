58c7893d7ac2e28311c7cbd04e41bef30decdf1b
ennni <djfwkd35@gamil.com>
2024-06-17 23:01:24 +0900

    jar 0.1.1 update version

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
+LOG_FILE=/home/ubuntu/build/before_install.log
+
+# Ensure the log file exists
+if [ ! -f "$LOG_FILE" ]; then
+  mkdir -p /home/ubuntu/build
+  touch $LOG_FILE
+fi
+
 # Before install script
-echo "Before install script executed at $(date)" >> /home/ubuntu/build/before_install.log
+echo "Before install script executed at $(date)" >> $LOG_FILE
 
 # 필요한 디렉토리를 생성
-mkdir -p /home/ubuntu/first-team-project-dev
-mkdir -p /home/ubuntu/first-team-project-dev/scripts
+mkdir -p /home/ubuntu/build
+mkdir -p /home/ubuntu/build/scripts
 
-echo "Directories created successfully" >> /home/ubuntu/build/before_install.log
+echo "Directories created successfully" >> $LOG_FILE
