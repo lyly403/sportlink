dada409f6464d645e1693353634dea693a7356c6
eeeeeni <140162964+eeeeeni@users.noreply.github.com>
2024-06-17 14:10:32 +0900

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -2,3 +2,9 @@
 
 # Before install script
 echo "Before install script executed at $(date)"
+
+# 필요한 디렉토리를 생성
+mkdir -p /home/ubuntu/first-team-project-dev
+mkdir -p /home/ubuntu/first-team-project-dev/scripts
+
+echo "Directories created successfully"
