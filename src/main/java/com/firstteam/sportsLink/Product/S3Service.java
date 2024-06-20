0f5635508ea47df8f5a6e76d71f49016d133ed0d
sangwoo_K <lyly403@daum.net>
2024-06-20 14:44:32 +0900

    Spring Cloud Amazon Bean 추가

--- src/main/java/com/firstteam/sportsLink/Product/S3Service.java
+++ src/main/java/com/firstteam/sportsLink/Product/S3Service.java
@@ -4,7 +4,6 @@ import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -14,25 +13,20 @@ import java.util.UUID;
 @Service
 public class S3Service {
 
-
     @Autowired
     private AmazonS3 amazonS3;
 
-    private final String bucketName = "sportlink-image";
+    private String bucketName = "sportlink-image";
 
+    public String uploadFile(MultipartFile file) throws IOException {
+        String fileName = generateFileName(file);
+        ObjectMetadata metadata = new ObjectMetadata();
+        metadata.setContentLength(file.getSize());
+        amazonS3.putObject(new PutObjectRequest(bucketName, fileName, file.getInputStream(), metadata));
+        return fileName;
+    }
 
-
-
-
-        public String uploadFile(MultipartFile file) throws IOException {
-            String fileName = generateFileName(file);
-            ObjectMetadata metadata = new ObjectMetadata();
-            metadata.setContentLength(file.getSize());
-            amazonS3.putObject(new PutObjectRequest(bucketName, fileName, file.getInputStream(), metadata));
-            return fileName;
-        }
-
-        private String generateFileName(MultipartFile file) {
-            return UUID.randomUUID().toString() + "-" + file.getOriginalFilename();
-        }
+    private String generateFileName(MultipartFile file) {
+        return UUID.randomUUID().toString() + "-" + file.getOriginalFilename();
+    }
 }
