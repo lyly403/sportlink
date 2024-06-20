37e04e1ce609aa4955f273d8a3426da825c17a7b
sangwoo_K <lyly403@daum.net>
2024-06-20 13:45:21 +0900

    s3 이미지 업로드 부분 수정

--- src/main/java/com/firstteam/sportsLink/SportsLinkApplication.java
+++ src/main/java/com/firstteam/sportsLink/SportsLinkApplication.java
@@ -2,8 +2,10 @@ package com.firstteam.sportsLink;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.aws.context.config.annotation.EnableContextInstanceData;
 import org.springframework.context.annotation.ComponentScan;
 
+@EnableContextInstanceData
 @SpringBootApplication
 public class SportsLinkApplication {
 	public static void main(String[] args) {
