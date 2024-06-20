b6596dcd097d387c15dccb1d81a024652384f3d8
sangwoo_K <lyly403@daum.net>
2024-06-20 17:05:55 +0900

    Spring Cloud Amazon Bean 추가, S3 service 수정, 오류 메세지 추가

--- src/main/java/com/firstteam/sportsLink/Product/S3Service.java
+++ src/main/java/com/firstteam/sportsLink/Product/S3Service.java
@@ -20,33 +20,29 @@ public class S3Service {
 
     private String bucketName = "sportlink-image";
 
-    public String uploadFile(MultipartFile file) throws IOException {
-        String fileName = file.getOriginalFilename();
-        ObjectMetadata metadata = new ObjectMetadata();
-        metadata.setContentLength(file.getSize());
-        metadata.setContentType(file.getContentType());
+    public String uploadFile(MultipartFile file) {
         try {
+            String fileName = generateFileName(file);
+            ObjectMetadata metadata = new ObjectMetadata();
+            metadata.setContentLength(file.getSize());
+
             InputStream inputStream = file.getInputStream();
             amazonS3.putObject(new PutObjectRequest(bucketName, fileName, inputStream, metadata));
             return fileName;
         } catch (IOException e) {
-            // 파일의 InputStream을 가져오는 중에 발생하는 예외 처리
             e.printStackTrace();
-            // 추가적인 예외 처리 로직을 추가할 수 있습니다.
-            return null;
+            return "Failed to upload file: " + e.getMessage();
         } catch (AmazonServiceException e) {
-            // Amazon S3 서비스에서 발생하는 예외 처리
             e.printStackTrace();
-            // S3 서비스의 오류 상태 코드와 메시지를 출력할 수 있습니다.
-            System.err.println("AmazonServiceException: " + e.getMessage());
-            return null;
+            return "Amazon S3 service exception: " + e.getMessage();
         } catch (SdkClientException e) {
-            // AWS SDK 클라이언트에서 발생하는 예외 처리
             e.printStackTrace();
-            // AWS SDK 클라이언트의 오류 상태 코드와 메시지를 출력할 수 있습니다.
-            System.err.println("SdkClientException: " + e.getMessage());
-            return null;
+            return "AWS SDK client exception: " + e.getMessage();
         }
+    }
+
+    private String generateFileName(MultipartFile file) {
+        return file.getOriginalFilename();
     }
 
 
