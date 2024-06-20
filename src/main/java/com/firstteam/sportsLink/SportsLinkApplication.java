582d61b3f335a766a329bc0ba3a88fbf3cde1a0c
sangwoo_K <lyly403@daum.net>
2024-06-20 15:05:54 +0900

    Spring Cloud Amazon Bean 추가

--- src/main/java/com/firstteam/sportsLink/SportsLinkApplication.java
+++ src/main/java/com/firstteam/sportsLink/SportsLinkApplication.java
@@ -2,10 +2,8 @@ package com.firstteam.sportsLink;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.aws.context.config.annotation.EnableContextInstanceData;
-import org.springframework.context.annotation.ComponentScan;
 
-@EnableContextInstanceData
+
 @SpringBootApplication
 public class SportsLinkApplication {
 	public static void main(String[] args) {
